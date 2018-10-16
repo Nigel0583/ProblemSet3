cbca9ccdb39ffefbc78548bb40180402040dfbe1
Nigel Guerin <nigel.guerin@students.ittralee.ie>
2018-10-16 17:27:43 +0100

    Problem set 3

---
+++ src/BankAccount.java
@@ -0,0 +1,21 @@
+public class BankAccount {
+    private String name;
+    private int accnum;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAccnum() {
+        return accnum;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAccnum(int accnum) {
+        this.accnum = accnum;
+    }
+}
