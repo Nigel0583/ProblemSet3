857e2bb3a841da7540f5dfe9a7653e844beea146
Nigel Guerin <nigel.guerin@students.ittralee.ie>
2018-10-16 17:19:27 +0100

    Problem set 3

--- src/House.java
+++ src/House.java
@@ -3,6 +3,7 @@ public class House {
     private String type;
     private  double price;
     private String owner;
+    private Layout layout;
 
     public String getAddress() {
         return address;
@@ -18,6 +19,10 @@ public class House {
 
     public String getOwner() {
         return owner;
+    }
+
+    public Layout getLayout() {
+        return layout;
     }
 
     public void setAddress(String address) {
@@ -36,24 +41,34 @@ public class House {
         this.owner = owner;
     }
 
+    public void setLayout(int rooms, int floors){
+        layout.setLayout(rooms,floors);
+    }
+
     //toString
     public String toString() {
-        return "Address " +getAddress()+ "\nType " +getType()+ "\nPrice " +getPrice()+ "\nOwner " +getOwner();
+        return "Address " +getAddress()+ "\nType " +getType()+ "\nPrice " +getPrice()+
+                "\nOwner " +getOwner()+ "\n" +layout.toString();
     }
 
     //3-argument constructor
-    public House(String address, String type, double price, String owner){
+    public House(String address, String type, double price, String owner, int floor, int rooms){
       setAddress(address);
       setType(type);
       setPrice(price);
       setOwner(owner);
-
+        layout = new Layout(floor,rooms);
     }
 
     //no argument constructor
     public House(){
-        this("No Address Specified", "No Type Specified", 0.0,"No owner specified");
+       address = "Not Specified";
+       type = "Not Specified";
+       price = 0;
+       owner = "Not Specified";
     }
+
+
 
 }
 
--- src/HouseDriver.java
+++ src/HouseDriver.java
@@ -3,7 +3,7 @@ public class HouseDriver {
 
 
         House house1 = new House();
-        House house2 = new House("kerry","2story",55634,"Owner");
+        House house2 = new House("kerry","2story",6546,"Owner",2,10);
 
        System.out.print(house2.toString());
     }
