2e3c7c10b66163f3b3da46eaeaa3a08224075acf
Nigel Guerin <nigel.guerin@students.ittralee.ie>
2018-10-16 16:42:53 +0100

    Problem set 3

--- src/House.java
+++ src/House.java
@@ -2,5 +2,59 @@ public class House {
     private String address;
     private String type;
     private  double price;
-    private Person owner;
+    private String owner;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    //toString
+    public String toString() {
+        return "Address " +getAddress()+ "\nType " +getType()+ "\nPrice " +getPrice()+ "\nOwner " +getOwner();
+    }
+
+    //3-argument constructor
+    public House(String address, String type, double price, String owner){
+      setAddress(address);
+      setType(type);
+      setPrice(price);
+      setOwner(owner);
+
+    }
+
+    //no argument constructor
+    public House(){
+        this("No Address Specified", "No Type Specified", 0.0,"No owner specified");
+    }
+
 }
+
+
---
+++ src/HouseDriver.java
@@ -0,0 +1,10 @@
+public class HouseDriver {
+    public static void main(String[] args) {
+
+
+        House house1 = new House();
+        House house2 = new House("kerry","2story",55634,"Owner");
+
+       System.out.print(house2.toString());
+    }
+}
